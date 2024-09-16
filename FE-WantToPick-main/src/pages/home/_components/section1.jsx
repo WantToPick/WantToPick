b58ea09b9fd8a30e6ad387deac7020f2b7848219
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-09-16 14:27:29 +0900

    section1 수정

--- FE-WantToPick-main/src/pages/home/_components/section1.jsx
+++ FE-WantToPick-main/src/pages/home/_components/section1.jsx
@@ -16,32 +16,28 @@ export default function Section1() {
             slidesPerView={1}
             loop={true}
             autoplay={{
-                delay: 3000,
+                delay: 5000,
                 disableOnInteraction: false,
             }}
             navigation
             pagination={{
                 clickable: true,
-                dynamicBullets: true, // 슬라이드 위치 표현하는 점 추가
+                dynamicBullets: true,
             }}
             modules={[Pagination, Autoplay]}
         >
-            {/* 첫 번째 슬라이드 */}
             <SwiperSlide>
                 <Slide1 />
             </SwiperSlide>
 
-            {/* 두 번째 슬라이드 */}
             <SwiperSlide>
                 <Slide2 />
             </SwiperSlide>
 
-            {/* 세 번째 슬라이드 */}
             <SwiperSlide>
                 <Slide3 />
             </SwiperSlide>
 
-            {/* 네 번째 슬라이드 */}
             <SwiperSlide>
                 <Slide4 />
             </SwiperSlide>
