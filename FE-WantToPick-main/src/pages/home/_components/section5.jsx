b6fb769be1ebae694b2090b466efa6181d6a5b7b
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-09-16 18:10:51 +0900

    홈 section5 수정

--- FE-WantToPick-main/src/pages/home/_components/section5.jsx
+++ FE-WantToPick-main/src/pages/home/_components/section5.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import checkIcon from '../../../assets/images/home/signin1.png'; // 체크 아이콘 이미지 경로
-import diamondIcon from '../../../assets/images/home/signin2.png'; // 다이아몬드 아이콘 이미지 경로
+import { routes } from '../../../constants/routes';
 
 const CircleArrowIcon = () => (
     <svg
@@ -22,18 +22,16 @@ export default function Section5() {
             <div className="container mx-auto text-center">
                 <h2 className="text-3xl font-bold mb-16">이제, 회원가입하고<br />나만의 연습생 포트폴리오를 만들어보세요</h2>
                 <div className="flex justify-center text-left space-x-8">
-                    <div className="bg-[#CDD5FF] p-10 rounded-3xl w-3/5 relative flex flex-col justify-between">
+                    <a href={routes.signUp} className="bg-[#CDD5FF] p-10 rounded-3xl w-3/5 relative flex flex-col justify-between">
                         <div>
                             <h3 className="text-[#526DF8] text-lg mb-2">나에게 딱 맞는 포트폴리오를 만들고 싶다면 ?</h3>
                             <h4 className="text-[#153BFF] text-2xl font-bold mb-4">회원가입하고<br />나만의 포트폴리오 만들기</h4>
-                            <div className="text-[#526DF8]">
-                                <a href="#" className="flex items-center">
-                                    <CircleArrowIcon />
-                                </a>
+                            <div className="text-[#526DF8] flex items-center">
+                                <CircleArrowIcon />
                             </div>
                         </div>
                         <img src={checkIcon} alt="Check Icon" className="self-end w-20 mt-14" />
-                    </div>
+                    </a>
                 </div>
             </div>
         </section>
