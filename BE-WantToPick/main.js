499fac288333ff27d6ac413288c6722ab9d2d073
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-09-18 23:40:10 +0900

    회원가입 백/프론트 연결 성공

--- BE-WantToPick/main.js
+++ BE-WantToPick/main.js
@@ -1,11 +1,12 @@
+const connectDB = require('./lib/db.js'); // db.js에서 MongoDB 연결 함수 가져오기
+
 require('dotenv').config(); // 환경 변수 로드
 const express = require('express');
-const mongoose = require('mongoose');
 const session = require('express-session');
 const cors = require('cors');
 const sessionConfig = require('./config/session'); // 세션 설정
-const userRoutes = require('./routes/userlogin'); // 사용자 라우터
-const authRoutes = require('./routes/auth'); // 인증 라우터 (필요 시 주석 해제)
+const sessionRoutes = require('./routes/session'); // 세션에 데이터 저장하는 라우터
+const signUpRoutes = require('./routes/signUp'); // /signin 라우터
 
 const app = express();
 
@@ -26,25 +27,11 @@ app.use(express.json()); // JSON 형식의 요청을 파싱
 app.use(express.urlencoded({ extended: true })); // URL 인코딩된 요청을 파싱
 app.use(session(sessionConfig)); // 세션 설정
 
-// MongoDB 연결 함수
-const connectDB = async () => {
-    try {
-        await mongoose.connect(MONGO_URI, {
-            //useNewUrlParser: true,
-            //useUnifiedTopology: true
-        });
-        console.log('MongoDB 연결 성공');
-    } catch (err) {
-        console.error('MongoDB 연결 실패:', err);
-    }
-};
-
-// MongoDB 연결 호출
 connectDB();
 
 // 라우터 설정
-app.use('/api/auth', authRoutes); // 인증 라우터 (필요 시 주석 해제)
-app.use('/api/user', userRoutes); // 사용자 라우터
+app.use('/api/session', sessionRoutes); // 세션에 데이터 저장 라우터
+app.use('/api', signUpRoutes); // /sign_up 라우터
 
 // 서버 시작
 app.listen(PORT, () => {
--- BE-WantToPick/models/user.js
+++ BE-WantToPick/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    birthdate: { type: Date, required: true },
+    birthdate: { type: String, required: true },
     gender: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
