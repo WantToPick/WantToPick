3f38608b47e809d1cf174cd8d7c2def2c10115b7
정수진 <124863799+ds-sujin@users.noreply.github.com>
2024-09-20 20:33:40 +0900

    models 스키마 user, selfintroduction, profile 모두 수정

--- BE-WantToPick/models/selfIntroduction.js
+++ BE-WantToPick/models/selfIntroduction.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
 const selfIntroductionSchema = new mongoose.Schema({
+  username: { type: String, required: true }, // username을 외래키로 사용
   training_period: { type: String, required: true },
   experience: { type: String, required: true },
   nationality: { type: String, required: true },
   motivation: { type: String, required: true },
   mbti: { type: String, required: true },
-  favorite_songs: { type: Object, required: true },
+  favorite_songs: { // favorite_songs를 객체로 정의
+    song1: { type: String, required: true },
+    song2: { type: String, required: true }
+  },
   role_model: { type: String, required: true },
   specialty: { type: String, required: true },
   charm_points: { type: String, required: true },
--- BE-WantToPick/models/user.js
+++ BE-WantToPick/models/user.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
     birthdate: { type: String, required: true },
     gender: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true }, // Username added
     password: { type: String, required: true }
 });
 
