d4313dcd77dc91a21ca17c0dd73d5caa5c1ba834
정수진 <124863799+ds-sujin@users.noreply.github.com>
2024-09-20 19:48:57 +0900

    user.js id 기본키로 수정

--- BE-WantToPick/models/user.js
+++ BE-WantToPick/models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    _id: { type: String, required: true }, // Custom user ID as the primary key
     name: { type: String, required: true },
     birthdate: { type: String, required: true },
     gender: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
-const User = mongoose.model('User', userSchema, 'userdb'); // userdb 컬렉션에 연결되도록
+const User = mongoose.model('User', userSchema, 'userdb'); // userdb collection
 
 module.exports = User;
