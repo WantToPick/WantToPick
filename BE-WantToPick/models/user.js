05de26aa26aec0924ed643fc7393959d7d21e5e9
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-09-20 21:09:03 +0900

    회원가입/로그인 에러 해결

--- BE-WantToPick/models/user.js
+++ BE-WantToPick/models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    _id: { type: String, required: true }, // Custom user ID as the primary key
     name: { type: String, required: true },
     birthdate: { type: String, required: true },
     gender: { type: String, required: true },
