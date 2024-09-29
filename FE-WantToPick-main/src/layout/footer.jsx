870178617435ab24b4012ec371c28e65b75799f4
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-09-30 00:15:59 +0900

    홈페이지

--- FE-WantToPick-main/src/layout/footer.jsx
+++ FE-WantToPick-main/src/layout/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <div className="bg-black h-[60px] w-full mt-96">
-            {/* Footer content here */}
+        <div className="bg-black h-[60px] w-full mt-96 text-[#5b5b5b] text-sm pl-10 flex items-center">
+            Copyright 2022 Company Name.
         </div>
     );
 }
