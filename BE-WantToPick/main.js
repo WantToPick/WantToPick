2cc884ae870824fb8e0393527c956c50d71f871e
heexji <nitej394@naver.com>
2024-09-29 13:47:57 +0900

    mongodb 연결 오류 해결

--- BE-WantToPick/main.js
+++ BE-WantToPick/main.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require('express');
-<<<<<<< HEAD
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const connectDB = require('./lib/db'); // MongoDB 연결 파일
+const mongoose = require('mongoose'); // mongoose를 가져옵니다.
+const mongoURI = process.env.MONGO_URI;
 const multer = require('multer'); // multer 추가
 const { exec } = require('child_process'); // child_process 추가
 const sessionConfig = require('./config/session'); // 세션 설정
@@ -15,17 +17,6 @@ const loginRoutes = require('./routes/login'); // 새로운 로그인 경로
 const selfIntroductionRoutes = require('./routes/selfIntroduction');
 const videoRoutes = require('./routes/video'); // 비디오 업로드/다운로드 라우터 추가
 const musicRoutes = require('./routes/trainingRoom'); // 노래 업로드/다운로드 라우터 추가
-=======
-const mongoose = require('mongoose');
-const cors = require('cors'); // CORS 패키지 추가
-const multer = require('multer'); // Multer 패키지 추가
-const { spawn } = require('child_process'); // child_process 패키지 추가
-const path = require('path'); // 경로 설정을 위한 패키지 추가
-require('dotenv').config(); // 환경 변수 설정
-
-// MongoDB URI
-const mongoURI = process.env.MONGO_URI; // 환경 변수에서 MongoDB URI 가져오기
->>>>>>> d15092a749cd6d245cff010796524c8ffa6e2a9c
 
 const app = express();
 
@@ -38,21 +29,8 @@ mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-<<<<<<< HEAD
 // MongoDB 연결
 connectDB(); // MongoDB 연결 함수 호출
-=======
-// Multer 설정
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // 파일을 저장할 경로
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname); // 원래 파일 이름 유지
-    },
-});
-const upload = multer({ storage });
->>>>>>> d15092a749cd6d245cff010796524c8ffa6e2a9c
 
 // /api/training 엔드포인트
 app.post('/api/training', upload.single('file'), (req, res) => {
